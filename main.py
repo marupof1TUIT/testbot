e582bdbb8cf624029740666a4a88724e69f685fa
Xusniddin <kursantmarufov@gmail.com>
2024-03-05 16:56:02 +0500

    test bot

---
+++ main.py
@@ -0,0 +1,14 @@
+#1
+# from collections import namedtuple
+#
+# Book = namedtuple('Book','title author janr year')
+#
+# books = [ ( 'O\'tgan kunlar','Abdulla Qodirov','Ishqiy','1992' ),
+#           ('Shaytanat','O\'tkir Hoshimov','Jangari','1995'),
+#           ('Yuksak ma\'naviyat yengilmas kuch','Islom Kaimov','Siyosiy','2012')     ]
+#
+# for book in books:
+#     book1 = Book(*book)
+# print(book1.author, book1.year)
+# -----------------------------------------------------------------------------------
+#2
